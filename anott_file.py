2f2e024e965ce3f7d5a0e31ecdbd9a8df7ab38d5
Daniel <shustovdaneil116@gmail.com>
2024-08-21 14:26:23 +0400

    annotation of types demo-file

---
+++ anott_file.py
@@ -0,0 +1,16 @@
+#testing function
+def say_something(number: int, message: str):
+    message = message.capitalize()
+    return message * number
+
+print(say_something(3, 'hello'))
+
+
+
+
+
+
+
+
+
+
